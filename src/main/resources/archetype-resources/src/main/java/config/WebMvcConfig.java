1fbd921c0b9b86ce6fb6641c2c2709f1a55ce339
Rafal Borowiec <rafal.borowiec@gmail.com>
2012-02-17 00:23:15 +0100

    WebMvcConfig changes: configuration of messages source added, JSR303 validator initialized with previously defined message source (messages.properties may be used for javax.validation messages and custom messages)

--- src/main/resources/archetype-resources/src/main/java/config/WebMvcConfig.java
+++ src/main/resources/archetype-resources/src/main/java/config/WebMvcConfig.java
@@ -1,6 +1,10 @@
 package ${package}.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.*;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.tiles2.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles2.TilesViewResolver;
@@ -11,11 +15,20 @@ import org.springframework.web.servlet.view.tiles2.TilesViewResolver;
 @Import(PersistenceConfig.class)
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	
+	private static final String MESSAGE_SOURCE = "/WEB-INF/classes/messages";
 	private static final String TILES = "/WEB-INF/tiles/tiles.xml";
 	private static final String VIEWS = "/WEB-INF/views/**/views.xml";
 	
 	private static final String RESOURCES_HANDLER = "/resources/";
 	private static final String RESOURCES_LOCATION = RESOURCES_HANDLER + "**";
+	
+	@Bean(name = "messageSource")
+	public MessageSource configureMessageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename(MESSAGE_SOURCE);
+		messageSource.setCacheSeconds(5);
+		return messageSource;
+	}
 	
 	@Bean
 	public TilesViewResolver configureTilesViewResolver() {
@@ -27,6 +40,13 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 		TilesConfigurer configurer = new TilesConfigurer();
 		configurer.setDefinitions(new String[] {TILES, VIEWS});
 		return configurer;
+	}
+	
+	@Override
+	public Validator getValidator() {
+		LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+		validator.setValidationMessageSource(configureMessageSource());
+		return validator;
 	}
 	
 	@Override
--- src/main/resources/archetype-resources/src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/resources/archetype-resources/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,7 +1,8 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s"%>
 <h1>Welcome to the Spring MVC application!</h1>
 
-<h2>Log entries</h2>
+<h2><s:message code="view.index.title" /></h2>
 <ul>
 	<c:forEach var="logEntry" items="${logEntries}">
 		<li>${logEntry.date} : ${logEntry.message}</li>
