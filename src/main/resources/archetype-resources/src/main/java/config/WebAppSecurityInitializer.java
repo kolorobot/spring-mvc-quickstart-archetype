3c27a7c9feddb14470aceedb17f9f3df3578677d
kolorobot <rafal.borowiec@gmail.com>
2014-03-06 00:24:46 +0100

    Encoding filter must be registered before security filter - fixed.

--- src/main/resources/archetype-resources/src/main/java/config/WebAppSecurityInitializer.java
+++
@@ -1,1 +0,0 @@
-package ${package}.config;import org.springframework.core.annotation.Order;import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;@Order(1)public class WebAppSecurityInitializer extends AbstractSecurityWebApplicationInitializer {}
