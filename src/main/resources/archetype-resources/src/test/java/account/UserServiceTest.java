cb54cbed57a628af2ae3bfa6e1afdc8cf9cdc63c
kolorobot <rafal.borowiec@gmail.com>
2014-03-05 20:06:35 +0100

    assertj instead of FEST, updated Spring librarires to newest versions

--- src/main/resources/archetype-resources/src/test/java/account/UserServiceTest.java
+++ src/main/resources/archetype-resources/src/test/java/account/UserServiceTest.java
@@ -1,6 +1,6 @@
 package ${package}.account;
 
-import static org.fest.assertions.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
