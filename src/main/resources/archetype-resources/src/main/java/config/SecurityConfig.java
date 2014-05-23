86261681bb423b3abbb60294d6f059e15d757d9e
Chris Hansen <hansen.chris.w@gmail.com>
2014-05-22 20:30:27 -0600

    Apply @dooman87's fix for #51.

--- src/main/resources/archetype-resources/src/main/java/config/SecurityConfig.java
+++ src/main/resources/archetype-resources/src/main/java/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package ${package}.config;
 
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.context.annotation.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import ${package}.account.UserService;
 
@@ -25,4 +30,40 @@ class SecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new StandardPasswordEncoder();
 	}
+
+  @Profile("test")
+  @Bean(name = "csrfMatcher")
+  public RequestMatcher testCsrfMatcher() {
+    return new RequestMatcher() {
+
+      @Override
+      public boolean matches(HttpServletRequest request) {
+        return false;
+      }
+    };
+  }
+
+  @Profile("!test")
+  @Bean(name = "csrfMatcher")
+  public RequestMatcher csrfMatcher() {
+    /**
+     * Copy of default request matcher from
+     * CsrfFilter$DefaultRequiresCsrfMatcher
+     */
+    return new RequestMatcher() {
+      private Pattern allowedMethods = Pattern
+        .compile("^(GET|HEAD|TRACE|OPTIONS)$");
+
+      /*
+       * (non-Javadoc)
+       *
+       * @see
+       * org.springframework.security.web.util.matcher.RequestMatcher#
+       * matches(javax.servlet.http.HttpServletRequest)
+       */
+      public boolean matches(HttpServletRequest request) {
+        return !allowedMethods.matcher(request.getMethod()).matches();
+      }
+    };
+  }
 }
