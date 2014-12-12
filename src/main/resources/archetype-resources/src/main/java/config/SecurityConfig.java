efc71380d36acc9532a0e3331fd414d0f127dcc5
kolorobot <rafal.borowiec@gmail.com>
2014-12-13 00:11:11 +0100

    #36 Got rid of XML security configuration.

--- src/main/resources/archetype-resources/src/main/java/config/SecurityConfig.java
+++ src/main/resources/archetype-resources/src/main/java/config/SecurityConfig.java
@@ -1,28 +1,63 @@
 package ${package}.config;
 
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
-
-import ${package}.account.UserService;
+import pl.codeleak.arch.account.UserService;
 
 @Configuration
-@ImportResource(value = "classpath:spring-security-context.xml")
-class SecurityConfig {
-	
-	@Bean
-	public UserService userService() {
-		return new UserService();
-	}
+@EnableWebMvcSecurity
+class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	public TokenBasedRememberMeServices rememberMeServices() {
-		return new TokenBasedRememberMeServices("remember-me-key", userService());
-	}
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new StandardPasswordEncoder();
-	}
+    @Bean
+    public UserService userService() {
+        return new UserService();
+    }
+
+    @Bean
+    public TokenBasedRememberMeServices rememberMeServices() {
+        return new TokenBasedRememberMeServices("remember-me-key", userService());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new StandardPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .eraseCredentials(true)
+                .userDetailsService(userService())
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/favicon.ico", "/resources/**", "/signup").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/signin")
+                .permitAll()
+                .failureUrl("/signin?error=1")
+                .loginProcessingUrl("/authenticate")
+                .and()
+            .logout()
+                .logoutUrl("/logout")
+                .permitAll()
+                .logoutSuccessUrl("/signin?logout")
+                .and()
+            .rememberMe()
+                .rememberMeServices(rememberMeServices())
+                .key("remember-me-key");
+    }
 }
