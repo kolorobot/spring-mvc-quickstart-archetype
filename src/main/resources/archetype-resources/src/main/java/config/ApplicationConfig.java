a5f7b0bb8e84770f32671b1206b20acb7273e0d3
kolorobot <rafal.borowiec@gmail.com>
2014-12-13 10:56:08 +0100

    Line ending set to LF

--- src/main/resources/archetype-resources/src/main/java/config/ApplicationConfig.java
+++ src/main/resources/archetype-resources/src/main/java/config/ApplicationConfig.java
@@ -1,1 +1,25 @@
-package ${package}.config;import static org.springframework.context.annotation.ComponentScan.Filter;import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;import org.springframework.context.annotation.Bean;import org.springframework.context.annotation.ComponentScan;import org.springframework.context.annotation.Configuration;import org.springframework.core.io.ClassPathResource;import org.springframework.stereotype.Controller;import ${package}.Application;@Configuration@ComponentScan(basePackageClasses = Application.class, excludeFilters = @Filter({Controller.class, Configuration.class}))class ApplicationConfig {		@Bean	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();		ppc.setLocation(new ClassPathResource("/persistence.properties"));		return ppc;	}	}
+package ${package}.config;
+
+import static org.springframework.context.annotation.ComponentScan.Filter;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Controller;
+
+import ${package}.Application;
+
+@Configuration
+@ComponentScan(basePackageClasses = Application.class, excludeFilters = @Filter({Controller.class, Configuration.class}))
+class ApplicationConfig {
+	
+	@Bean
+	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+		ppc.setLocation(new ClassPathResource("/persistence.properties"));
+		return ppc;
+	}
+	
+}
