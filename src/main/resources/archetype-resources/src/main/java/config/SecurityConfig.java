7f5a6a531cfe38c4a0c0f580ac730e5a306951ce
kolorobot <rafal.borowiec@gmail.com>
2014-12-13 10:08:46 +0100

    Formatting

--- src/main/resources/archetype-resources/src/main/java/config/SecurityConfig.java
+++ src/main/resources/archetype-resources/src/main/java/config/SecurityConfig.java
@@ -19,52 +19,52 @@ import ${package}.account.UserService;
 @EnableWebMvcSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	public TokenBasedRememberMeServices rememberMeServices() {
-		return new TokenBasedRememberMeServices("remember-me-key", userService());
+    @Bean
+    public TokenBasedRememberMeServices rememberMeServices() {
+        return new TokenBasedRememberMeServices("remember-me-key", userService());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new StandardPasswordEncoder();
 	}
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new StandardPasswordEncoder();
-	}
 
-  @Profile("test")
-  @Bean(name = "csrfMatcher")
-  public RequestMatcher testCsrfMatcher() {
-    return new RequestMatcher() {
+    @Profile("test")
+    @Bean(name = "csrfMatcher")
+    public RequestMatcher testCsrfMatcher() {
+        return new RequestMatcher() {
 
-      @Override
-      public boolean matches(HttpServletRequest request) {
-        return false;
-      }
-    };
-  }
+            @Override
+            public boolean matches(HttpServletRequest request) {
+                return false;
+            }
+        };
+    }
 
-  @Profile("!test")
-  @Bean(name = "csrfMatcher")
-  public RequestMatcher csrfMatcher() {
-    /**
-     * Copy of default request matcher from
-     * CsrfFilter$DefaultRequiresCsrfMatcher
-     */
-    return new RequestMatcher() {
-      private Pattern allowedMethods = Pattern
-        .compile("^(GET|HEAD|TRACE|OPTIONS)$");
+    @Profile("!test")
+    @Bean(name = "csrfMatcher")
+    public RequestMatcher csrfMatcher() {
+        /**
+         * Copy of default request matcher from
+         * CsrfFilter$DefaultRequiresCsrfMatcher
+         */
+        return new RequestMatcher() {
+            private Pattern allowedMethods = Pattern
+                    .compile("^(GET|HEAD|TRACE|OPTIONS)$");
 
-      /*
-       * (non-Javadoc)
-       *
-       * @see
-       * org.springframework.security.web.util.matcher.RequestMatcher#
-       * matches(javax.servlet.http.HttpServletRequest)
-       */
-      public boolean matches(HttpServletRequest request) {
-        return !allowedMethods.matcher(request.getMethod()).matches();
-      }
-    };
-  }
-    
+            /*
+             * (non-Javadoc)
+             *
+             * @see
+             * org.springframework.security.web.util.matcher.RequestMatcher#
+             * matches(javax.servlet.http.HttpServletRequest)
+             */
+            public boolean matches(HttpServletRequest request) {
+                return !allowedMethods.matcher(request.getMethod()).matches();
+            }
+        };
+    }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
