87a3463947915d9353bc1e801c1c4132e5bc4a24
kolorobot <rafal.borowiec@gmail.com>
2016-01-14 23:17:05 +0100

    Upgrade platform version to 2.0.1

--- src/main/resources/archetype-resources/src/main/java/config/WebMvcConfig.java
+++ src/main/resources/archetype-resources/src/main/java/config/WebMvcConfig.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-import org.thymeleaf.extras.springsecurity3.dialect.SpringSecurityDialect;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
